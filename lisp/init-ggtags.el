663de172ec0b2f5599b38cc764f9329aea3e7a2c
CestLolita <232191159@qq.com>
2017-10-10 11:24:41 +0800

    Change some key about ggtags

--- lisp/init-ggtags.el
+++ lisp/init-ggtags.el
@@ -5,9 +5,9 @@
     (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
       (ggtags-mode 1))))
 (define-key ggtags-mode-map (kbd "<f8>") 'ggtags-find-tag-regexp)
-(define-key ggtags-mode-map (kbd "M-j")  'ggtags-find-tag-dwim)
+(define-key ggtags-mode-map (kbd "M-j")  'ggtags-find-definition)
 (define-key ggtags-mode-map (kbd "M-r")  'ggtags-find-reference)
-(define-key ggtags-mode-map (kbd "M-d")  'ggtags-find-definition)
+(define-key ggtags-mode-map (kbd "M-.")  'ggtags-find-tag-dwim)
 (define-key ggtags-mode-map (kbd "C-b")  'ggtags-prev-mark)
 (define-key ggtags-mode-map (kbd "C-f")  'ggtags-next-mark)
 
