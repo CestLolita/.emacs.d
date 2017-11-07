38698e331fe52775ade255d1af17b21c65f83045
CestLolita <232191159@qq.com>
2017-11-07 10:37:46 +0800

    Change the complete configuration to default

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,8 +1,7 @@
 (require 'company)
 
 (add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay nil)
-(global-set-key (kbd "<f1>") 'company-indent-or-complete-common)
-;;(global-set-key (kbd "C-<tab>") 'company-indent-or-complete-common)
+;;(setq company-idle-delay nil)
+;;(global-set-key (kbd "<f1>") 'company-indent-or-complete-common)
 
 (provide 'init-company)
