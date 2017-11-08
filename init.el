12f1cf41d4ad8e8d0a89310bdab0735a9cce6883
CestLolita <232191159@qq.com>
2017-11-08 16:35:44 +0800

    Change another way for auto-load packages

--- init.el
+++ init.el
@@ -7,34 +7,43 @@
 
 ;;--------->Package managment<---------
 ;;List the repositories containing them
-(setq package-archives
-  '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
-    ("MELPA Stable" . "https://stable.melpa.org/packages/")
-    ("MELPA"        . "https://melpa.org/packages/"))
- package-archive-priorities
-  '(("MELPA Stable" . 10)
-    ("GNU ELPA"     . 5)
-    ("MELPA"        . 0)))
+(require 'package)
 
+;; activate installed packages
+;;(package-initialize)
+
+(setq package-archives
+      '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
+        ("MELPA Stable" . "https://stable.melpa.org/packages/")
+        ("MELPA"        . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("MELPA Stable" . 10)
+        ("GNU ELPA"     . 5)
+        ("MELPA"        . 0)))
+
+;; activate installed packages
 (package-initialize)
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+   Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+         nil
+         ;; (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+         ;;     (package-install package)
+         ;;     package)
+	 (package-install package)))
+   packages))
 
-(defvar my-packages '(
-  zenburn-theme
-  ivy
-  swiper
-  counsel
-  projectile
-  counsel-projectile
-  ggtags
-  highlight-symbol
-  company))
+;; make sure to have downloaded archive description.
+;; Or use package-archive-contents as suggested by Nicolas Dudebout
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(ensure-package-installed 'zenburn-theme 'ivy 'swiper 'counsel 'projectile 'counsel-projectile 'ggtags 'highlight-symbol 'company) ;  --> (nil nil) if iedit and magit are already installed
+
 
 ;;--------->lisp and site-lisp path<---------
 (add-to-list 'load-path (format "%s%s" emacs-home "site-lisp"))
