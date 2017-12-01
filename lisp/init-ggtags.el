9d5d115be176c6eb399379779a712ac1d1988fda
CestLolita <232191159@qq.com>
2017-12-01 15:21:43 +0800

    enable ggtags-mode under python-mode and ruby-mode

--- lisp/init-ggtags.el
+++ lisp/init-ggtags.el
@@ -4,6 +4,10 @@
   (lambda ()
     (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
       (ggtags-mode 1))))
+
+(add-hook 'python-mode-hook (lambda () (ggtags-mode 1)))
+(add-hook 'ruby-mode-hook (lambda () (ggtags-mode 1)))
+
 (define-key ggtags-mode-map (kbd "<f8>") 'ggtags-find-tag-regexp)
 (define-key ggtags-mode-map (kbd "M-j")  'ggtags-find-definition)
 (define-key ggtags-mode-map (kbd "M-r")  'ggtags-find-reference)
